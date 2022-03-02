1d1ee1fbae286efeee109e6acc1377cd738bf233
João Moraes <Joo Moraes@DESKTOP-2QU5RNS>
2022-03-02 13:43:44 -0300

    Add project files.

---
+++ FrequenciaNúmeros/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FrequenciaNúmeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 1, 2, 3, 7, 8, 10, 12, 10,1,10 };
+
+            Array.Sort(numeros);
+            string aparecer = "Apareceu";
+            string vezes = "";
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                int cont = 0;
+                
+               for (int j = 0; j < numeros.Length; j++)
+                {
+                    if (numeros[i]==numeros[j])
+                    {
+                        cont++;
+                        i = j;
+                    }
+                }
+               if(cont == 1)
+                {
+                    vezes = "vez";
+                }
+                else
+                {
+                    vezes = "vezes";
+                }
+                
+                Console.WriteLine($"{numeros[i]} {aparecer} {cont} {vezes}"  );
+            }
+
+        }
+    }
+}
